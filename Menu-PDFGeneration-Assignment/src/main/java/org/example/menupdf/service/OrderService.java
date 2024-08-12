3d71ed0dc5591917f532b3fd7ffbb62a6d473ee8
Swasthik-Shetty <Swasthik.Shetty@monetize360.com>
2024-08-13 00:04:22 +0530

    Menu Completed

--- Menu-PDFGeneration-Assignment/src/main/java/org/example/menupdf/service/OrderService.java
+++ Menu-PDFGeneration-Assignment/src/main/java/org/example/menupdf/service/OrderService.java
@@ -1,11 +1,12 @@
 package org.example.menupdf.service;
 
+import org.example.menupdf.domain.Menu;
 import org.example.menupdf.dto.OrderDTO;
 import org.example.menupdf.dto.OrderInfoDTO;
 
 import java.util.List;
 
 public interface OrderService {
-    public List<OrderDTO> processOrder();
+    public List<OrderDTO> processOrder(Menu menu);
     public OrderInfoDTO generateInvoice(List<OrderDTO> orders, String name);
 }
